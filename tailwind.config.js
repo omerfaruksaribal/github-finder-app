a758afee7438a40f62d7695e37aac27166dec4bc
omerfaruksaribal <omerfaruk_saribal@hotmail.com>
2024-07-09 16:00:54 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+      extend: {},
+  },
+  plugins: [require('daisyui')],
+  daisyui: {
+      themes: ['dark'],
+  },
+};
